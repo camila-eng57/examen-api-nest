3c07da40c1c828d8440b8ab516e9390bfaa3608f
Cami16100 <ccastillog2000@alumno.ipn.mx>
2026-05-03 23:23:59 -0600

    Agrega modulos a AppModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,14 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ConfigModule } from '@nestjs/config';
+import { UsersModule } from './users/users.module'; // <-- Aquí está
+import { AuthModule } from './auth/auth.module';   // <-- Y aquí
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
     TypeOrmModule.forRoot({
-      type: 'postgres', // Cambiado a postgres
+      type: 'postgres',
       host: process.env.DB_HOST,
       port: parseInt(process.env.DB_PORT || '24974', 10),
       username: process.env.DB_USERNAME,
@@ -14,13 +16,15 @@ import { ConfigModule } from '@nestjs/config';
       database: process.env.DB_NAME,
       autoLoadEntities: true,
       synchronize: true,
-      ssl: true, // Requerido por Aiven
+      ssl: true,
       extra: {
         ssl: {
           rejectUnauthorized: false,
         },
       },
     }),
+    UsersModule, // <-- Lo conectamos aquí
+    AuthModule,  // <-- Lo conectamos aquí
   ],
 })
 export class AppModule {}
